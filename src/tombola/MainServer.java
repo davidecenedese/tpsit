c26439082f9b8ccf7191eb90cd0e07fc05bd96ce
simonestella <simonestella@INFO1-PC09.itisvv4.local>
2017-12-16 10:50:37 +0100

    commit18

--- src/tombola/MainServer.java
+++ src/tombola/MainServer.java
@@ -14,25 +14,34 @@ import java.net.Socket;
  * @author simonestella
  */
 public class MainServer {
-    
+
     public void start() throws Exception{
-        try{
+        try {
             ServerSocket serverSocket = new ServerSocket(6789);
-            while(true){
+
+            /*final int secondi = 0;
+            new Runnable() {
+                @Override
+                public void run() {
+                    secondi++;
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+            };*/
+            while (true) {
                 System.out.println("1 Server in attesa...");
                 Socket socket = serverSocket.accept();
                 System.out.println("3 Server socket " + socket);
                 Server serverThread = new Server(socket);
                 serverThread.start();
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             System.out.println("Errore durante l'istanza del server");
             System.exit(1);
         }
     }
-    
-    public static void main(String[] args) throws IOException, Exception{
+
+    public static void main(String[] args) throws IOException, Exception {
         MainServer tcpServer = new MainServer();
         tcpServer.start();
     }
