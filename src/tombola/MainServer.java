2aeb264410a77800a529b7728eef2d122ba8fe9e
simonestella <simonestella@INFO1-PC09.itisvv4.local>
2017-12-16 10:33:11 +0100

    commit17

---
+++ src/tombola/MainServer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tombola;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author simonestella
+ */
+public class MainServer {
+    
+    public void start() throws Exception{
+        try{
+            ServerSocket serverSocket = new ServerSocket(6789);
+            while(true){
+                System.out.println("1 Server in attesa...");
+                Socket socket = serverSocket.accept();
+                System.out.println("3 Server socket " + socket);
+                Server serverThread = new Server(socket);
+                serverThread.start();
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istanza del server");
+            System.exit(1);
+        }
+    }
+    
+    public static void main(String[] args) throws IOException, Exception{
+        MainServer tcpServer = new MainServer();
+        tcpServer.start();
+    }
+}
