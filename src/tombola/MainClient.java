b6f77a8b5f879c8e067989295bd444711d04f74f
Davide Cenedese <Davide Cenedese@DESKTOP-DAVIDE>
2017-12-20 17:03:39 +0100

    2017-12-20 17.03

--- src/tombola/MainClient.java
+++ src/tombola/MainClient.java
@@ -18,7 +18,8 @@ public class MainClient {
     public static void main(String[] args) {
         Client client = new Client();
         client.connect();
-        client.comunicate();
+        client.receiveCard();
+        client.receiveNumbers();
     }
 
 }
