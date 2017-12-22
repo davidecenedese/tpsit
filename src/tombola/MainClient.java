e1048a1ddd505fd8ffc6fcf6fdf18cb41e1da04a
Davide Cenedese <Davide Cenedese@DESKTOP-DAVIDE>
2017-12-22 15:20:18 +0100

    2017-12-22 15:20

--- src/tombola/MainClient.java
+++ src/tombola/MainClient.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package tombola;
 
 /**
  *
- * @author Davide Cenedese
+ * @authors Cenedese, Stella
  */
 public class MainClient {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Client client = new Client();
         client.connect();
